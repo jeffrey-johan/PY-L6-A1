206b67660766ed74f0cdb30f91889fd69ce5b39c
jeffrey-johan <jeffreyjohan974@gmail.com>
2024-11-10 11:58:15 -0700

    l6 a1

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to turtle window")
+
+board = turtle.Turtle()
+
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i = i+1
+
+turtle.done()
